5ff2bd93f1f921995938b98e35f2f896169d3dd3
AROSOFT Innovations <arosoft@arosoft.io>
2026-04-06 16:12:17 +0300

    fix: change bcryptjs to bcrypt to match installed package

--- backend/src/controllers/admin.controller.ts
+++ backend/src/controllers/admin.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import bcrypt from 'bcryptjs';
+import bcrypt from 'bcrypt';
 import { db } from '../config/database';
 
 // @desc    Get all complaints (Admin/Staff only)
